3c80f5dca3f09999d93331ce4b1fbf720cc3b82e
Jaden Satheesh <25jsatheesh@cvsdstudents.org>
2024-01-08 08:22:44 -0500

    - Added #include std.lib

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,15 @@
-// cool text
+// Jaden Satheesh, Levy Le, Joel Underkoffler
+// Period 1
+// January 8th, 2023
+// Eight of Queens
 
-// change
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
