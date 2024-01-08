e9271f50ac82e03ca3186165dc455ddb734e1d3e
Joel Underkoffler <25junderkoffler@cvsdstudents.org>
2024-01-08 08:37:15 -0500

    basic print

--- Source.cpp
+++ Source.cpp
@@ -12,10 +12,11 @@ const int SIZE = 8;
 // Initialize the chessboard with all spaces empty (true = queen)
 bool board[SIZE][SIZE] = { false };
 
+void printBoard(bool board[SIZE][SIZE]);
 
 int main()
 {
-
+	printBoard(board);
 	return 0;
 }
 // change
@@ -23,3 +24,22 @@ int main()
 // another change
 
 // joel change
+
+void printBoard(bool board[SIZE][SIZE])
+{
+	for (int r = 0; r < SIZE; r++)
+	{
+		for (int c = 0; c < SIZE; c++)
+		{
+			if (board[r][c] == false)
+			{
+				cout << "0 ";
+			}
+			else
+			{
+				cout << "Q ";
+			}
+		}
+		cout << "\n";
+	}
+}
